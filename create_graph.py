0f0bc28b8abed61b8638e5d7aa8ef29e9b4f9295
Tiago Vaz <tvaz@riseup.net>
2024-01-28 17:58:42 -0500

    Improving graphs.

--- create_graph.py
+++ create_graph.py
@@ -18,7 +18,7 @@ def create_graph():
         for s in scales:
             if rrd == 'airgradient_1.rrd':
                 rrdtool.graph(f'{rrd.rsplit(".", 1)[0] + f"-{s}.png"}', \
-                               '-w720', '-h140', '-aPNG', \
+                               '-w820', '-h180', '-aPNG', \
                                '--start',  f'{resolutions[s]}', \
                                '--end', 'now', \
                                '--slope-mode', \
@@ -35,29 +35,46 @@ def create_graph():
                                f'DEF:pm={rrd}:pm:AVERAGE', \
                                f'DEF:co2={rrd}:co2:AVERAGE', \
                                f'CDEF:co2_calc=co2,0.01,*', \
-                               'AREA:pm#00FF00:PM2.5(ug/m)', \
-                               'LINE1:humid#FF00FF:Humid. (%)', \
+                               f'CDEF:voc_calc=voc,0.1,*', \
+                               'AREA:pm#ffd700:PM2.5(ug/m)', \
+                               'LINE2:humid#FF00FF:Humid. (%)', \
                                'LINE2:temp#0000FF:Temp. (C)', \
-                               'LINE4:co2_calc#00FFFF:CO2 (ppm/100)', \
-                               'LINE5:voc#ffb91d:VOC (idx)', \
-                               'LINE6:nox#c41e3a:NOx (idx)', \
-#                               'GPRINT:temp:LAST:\tTempCur\: %5.2lf', \
-#                               'GPRINT:temp:AVERAGE:TempAvg\: %5.2lf', \
-#                               'GPRINT:temp:MAX:TempMax\: %5.2lf', \
-#                               'GPRINT:temp:MIN:TempMin\: %5.2lf\\n', \
-#                               'GPRINT:pm:LAST:\t\t\t\t\t\t               PM25Cur\: %5.2lf', \
-#                               'GPRINT:pm:AVERAGE:PM25Avg\: %5.2lf', \
-#                               'GPRINT:pm:MAX:PM25Max\: %5.2lf', \
-#                               'GPRINT:pm:MIN:PM25Min\: %5.2lf\\n', \
-#                               'GPRINT:co2_calc:LAST:\t\t\t\t\t\t               CO2Cur\: %5.2lf', \
-#                               'GPRINT:co2_calc:AVERAGE: CO2Avg\: %5.2lf', \
-#                               'GPRINT:co2_calc:MAX: CO2Max\: %5.2lf', \
-#                               'GPRINT:co2_calc:MIN: CO2Min\: %5.2lf\\n', \
-                               
+                               'LINE2:co2_calc#FF0000:CO2 (ppm/100)', \
+                               'LINE2:voc_calc#20b2aa:VOC (idx/10)', \
+                               'LINE2:nox#654321:NOx (idx)', \
+                               'COMMENT:                 ', \
+                               'GPRINT:pm:LAST:CUR\: PM2.5\: %3.0lfug/m', \
+                               'GPRINT:humid:LAST:Humid\: %2.1lf%%', \
+                               'GPRINT:temp:LAST:Temp\: %2.1lfC', \
+                               'GPRINT:co2:LAST:CO2\: %4.0lfppm', \
+                               'GPRINT:voc:LAST:VOC\: %3.0lf', \
+                               'GPRINT:nox:LAST:NOx\: %3.0lf', \
+                               'COMMENT:                 ', \
+                               'GPRINT:pm:MIN:MIN\: PM2.5\: %3.0lfug/m', \
+                               'GPRINT:humid:MIN:Humid\: %2.1lf%%', \
+                               'GPRINT:temp:MIN:Temp\: %2.1lfC', \
+                               'GPRINT:co2:MIN:CO2\: %4.0lfppm', \
+                               'GPRINT:voc:MIN:VOC\: %3.0lf', \
+                               'GPRINT:nox:MIN:NOx\: %3.0lf', \
+                               'COMMENT:                 ', \
+                               'GPRINT:pm:MAX:MAX\: PM2.5\: %3.0lfug/m', \
+                               'GPRINT:humid:MAX:Humid\: %2.1lf%%', \
+                               'GPRINT:temp:MAX:Temp\: %2.1lfC', \
+                               'GPRINT:co2:MAX:CO2\: %4.0lfppm', \
+                               'GPRINT:voc:MAX:VOC\: %3.0lf', \
+                               'GPRINT:nox:MAX:NOx\: %3.0lf', \
+                               'COMMENT:                 ', \
+                               'GPRINT:pm:AVERAGE:AVG\: PM2.5\: %3.0lfug/m', \
+                               'GPRINT:humid:AVERAGE:Humid\: %2.1lf%%', \
+                               'GPRINT:temp:AVERAGE:Temp\: %2.1lfC', \
+                               'GPRINT:co2:AVERAGE:CO2\: %4.0lfppm', \
+                               'GPRINT:voc:AVERAGE:VOC\: %3.0lf', \
+                               'GPRINT:nox:MAX:NOx\: %3.0lf', \
+                               'COMMENT:                 ', \
                             )
             else:
                 rrdtool.graph(f'{rrd.rsplit(".", 1)[0] + f"-{s}.png"}', \
-                            '-w720', '-h140', '-aPNG', \
+                            '-w820', '-h180', '-aPNG', \
                             '--start',  f'{resolutions[s]}', \
                             '--end', 'now', \
                             '--slope-mode', \
@@ -70,7 +87,7 @@ def create_graph():
                             '--alt-y-grid', '--rigid', \
                             f'DEF:temp={rrd}:temp:AVERAGE', \
                             f'DEF:humid={rrd}:humid:AVERAGE', \
-                            'LINE1:humid#FF00FF:Humidity (%)', \
+                            'LINE2:humid#FF00FF:Humidity (%)', \
                             'LINE2:temp#0000FF:Temperature (C)', \
                             'GPRINT:temp:LAST:Cur\: %5.2lf', \
                             'GPRINT:temp:AVERAGE:Avg\: %5.2lf', \
